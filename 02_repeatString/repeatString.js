67a35a20b1b9e748852ea41a359395e782a27a10
Brent Kytchak <bkytchak@gmail.com>
2022-03-28 19:42:48 -0400

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = ""
+    if (num < 0){
+        return "ERROR"
+    } else if (string === ""){
+        return ""
+    } else {
+   
+    } while (num > 0){
+        newString += string
+        num--
+    }
+    return newString
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
