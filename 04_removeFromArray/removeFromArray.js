3d7506f2fabb103a6a58b51da961f422bbb8706c
Brent Kytchak <bkytchak@gmail.com>
2022-03-28 21:09:14 -0400

    completed exercise 3, but had to review solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args){
+    const array = args[0];
+    console.log(array)
+    const newArray = [];
+    console.log(newArray)
+    array.forEach((num) => { //For each num in array
+        if (!args.includes(num)){ //If num is not in the arguments
+            newArray.push(num); //Add it to the newArray
+            console.log(newArray)
+        }
+    });
+
+return newArray;
+   
 
 };
-
+console.log(removeFromArray([1,2,3,4,5,6],3,2))
 // Do not edit below this line
 module.exports = removeFromArray;
