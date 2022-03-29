340a7ab45cbc6908df673c50778cd3a10994c7ac
Brent A Kytchak <bkytchak@gmail.com>
2022-03-29 13:39:45 -0400

    Update sumAll.js
    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+  sum = 0
+  myArray = args
+  for(let i = 0; i < myArray.length; i++){
+    if (myArray[i] < 0) {
+      return "ERROR"
+    } else if (typeof myArray[i] === 'string'){
+      return "ERROR"
+    } else if (typeof myArray[i] === "object") {
+      return "ERROR"
+    } else {
+      sum += myArray[i]
+  }
+  }
+    
+  return sum 
 
 };
+
+  console.log(sumAll(2, 4, 6, 8, 9))
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
