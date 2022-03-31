d15acb59c8b66ec37224b89658b56e164a42fd62
Brent Kytchak <bkytchak@gmail.com>
2022-03-31 18:53:47 -0400

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //Creates a string in all lowercase and replaces in non-word characters
+   let lowString = string.toLowerCase().replace(/[\W_]/g,'')
+   //Reverses the original string by splitting into individual characters, reversing, then joining
+   let revString = lowString.split("").reverse().join("")
+   //Checks if the string is a palindrome
+   if (revString === lowString) {
+       return true;
+   } else {
+       return false;
+   };
+
 
 };
-
+console.log(palindromes("Race Car"))
 // Do not edit below this line
 module.exports = palindromes;
