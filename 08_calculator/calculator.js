7ebe52c6c26b2313e8e8b6b4fffc1b7b0ae24a78
Brent A Kytchak <bkytchak@gmail.com>
2022-03-31 15:19:41 -0400

    Update calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,37 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((a, b) => a + b, 0)
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b)
+
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return Math.pow(a, b);
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0 || n === 1) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--){
+    product = product * i
+   
+  }
+  return product
 	
 };
 
@@ -30,4 +43,11 @@ module.exports = {
   multiply,
   power,
   factorial
-};
+}; 
+
+console.log(add(2, 3))
+console.log(subtract(2,1))
+console.log(sum([1,2,3,4]))
+console.log(multiply([2,4,6,8,10,12,14]))
+console.log(power(2, 3))
+console.log(factorial(5))
