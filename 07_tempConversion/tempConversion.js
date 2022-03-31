4eb2ca0285c519efe9fa8689321e8f9b5555c0d5
Brent A Kytchak <bkytchak@gmail.com>
2022-03-31 14:39:52 -0400

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,23 @@
-const ftoc = function() {
+const ftoc = function(fTemp) {
+  newTemp = (fTemp - 32) * 0.555555556;
+  newTemp = newTemp.toFixed(1)
+  return parseFloat(newTemp)
+     
 
 };
 
-const ctof = function() {
+const ctof = function(cTemp) {
+  newTemp = (cTemp * 1.8) + 32;
+  newTemp = newTemp.toFixed(1);
+  return parseFloat(newTemp)
+  
 
 };
 
+console.log(ftoc(63))
+console.log(ctof(55))
 // Do not edit below this line
-module.exports = {
-  ftoc,
-  ctof
-};
+//module.exports = {
+  //ftoc,
+  //ctof
+//};
