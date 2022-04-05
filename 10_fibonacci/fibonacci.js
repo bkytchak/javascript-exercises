05001b450de86f3269b354c53dfe866bbe9f1f15
Brent Kytchak <bkytchak@gmail.com>
2022-04-04 20:00:02 -0400

    Completed more exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(count) {
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
+    let x = 0
+    let y = 1
+    for (let i = 1; i < count; i++){
+        const temp = y;
+        y = x + y;
+        x = temp;
+    }
+return y
+}
+console.log(fibonacci(4))
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let newArray = array.map(book => book.title)
+    return newArray
+    
 
 };
 
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+  console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
